92f0abe12825f80e3105ac3ea4501ee8262fd863
Shiffa Ur Rehman <77918908+ShiffaUrRehman@users.noreply.github.com>
2023-09-08 16:42:13 +0500

    Passwords saved via hashing + Login

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,6 @@
 const { User } = require("../model/userModel");
+const bcrypt = require("bcrypt");
+const SALT_WORK_FACTOR = 10;
 
 // @desc    Get all Users
 // @route   GET /api/user
@@ -6,6 +8,29 @@ const getUsers = async (req, res) => {
   try {
     const users = await User.find();
     res.status(200).send(users);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+// @desc    Login a User
+// @route   POST /api/user/login
+
+const login = async (req, res) => {
+  // !!!
+  // Check validation for this body
+  // !!!
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) res.status(404).send({ message: "User not found" });
+    else {
+      const isValid = await bcrypt.compare(req.body.password, user.password);
+      if (!isValid) {
+        res.status(404).send("Wrong Password");
+      } else {
+        res.status(200).send(user);
+      }
+    }
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
@@ -29,3 +54,4 @@ const createUser = async (req, res) => {
 
 module.exports.getUsers = getUsers;
 module.exports.createUser = createUser;
+module.exports.login = login;
